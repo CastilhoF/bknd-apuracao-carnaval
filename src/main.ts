727d69f36187ca8270e2e6a07c2ed41bf2b97f58
CastilhoF <castilho.wylde@gmail.com>
2022-04-13 16:37:20 -0300

    add: interceptor implemantation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TransformInterceptor());
+  await app.listen(process.env.PORT);
+  logger.log(`Application listening on port: ${process.env.PORT}`);
 }
 bootstrap();
