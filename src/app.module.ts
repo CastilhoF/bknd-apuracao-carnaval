a40ee17c751a1ce5fdeebe77f81190191a44b4bf
CastilhoF <castilho.wylde@gmail.com>
2022-04-14 01:07:14 -0300

    chore: change config module typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { configEnvironmentsValidation } from './config/validations/config.env.validation';
+import { configEnvironmentsValidation } from './core/config/validations/config.env.validation';
+import { TypeOrmConfig } from './core/config/database/typeorm.config';
 
 @Module({
   imports: [
@@ -9,22 +10,7 @@ import { configEnvironmentsValidation } from './config/validations/config.env.va
       envFilePath: [`.stage.${process.env.STAGE}.env`],
       validationSchema: configEnvironmentsValidation,
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => {
-        return {
-          type: 'mysql',
-          host: configService.get('HOST_MYSQL'),
-          port: configService.get('PORT_MYSQL'),
-          username: configService.get('USER_MYSQL'),
-          password: configService.get('PWD_MYSQL'),
-          database: configService.get('DB_MYSQL'),
-          autoLoadEntities: true,
-          synchronize: true,
-        };
-      },
-    }),
+    TypeOrmModule.forRootAsync(TypeOrmConfig),
   ],
 })
 export class AppModule {}
