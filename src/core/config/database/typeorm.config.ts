dff2b75fc0014c1d03048c1540e5de3cf9c287f8
CastilhoF <castilho.wylde@gmail.com>
2022-04-14 01:11:25 -0300

    chore: new config file for typeorm mysql

---
+++ src/core/config/database/typeorm.config.ts
@@ -0,0 +1,19 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+
+export const TypeOrmConfig: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: async (configService: ConfigService) => {
+    return {
+      type: 'mysql',
+      host: configService.get('HOST_MYSQL'),
+      port: configService.get('PORT_MYSQL'),
+      username: configService.get('USER_MYSQL'),
+      password: configService.get('PWD_MYSQL'),
+      database: configService.get('DB_MYSQL'),
+      autoLoadEntities: true,
+      synchronize: true,
+    };
+  },
+};
