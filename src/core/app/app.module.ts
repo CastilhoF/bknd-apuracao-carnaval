84bbfba16aa1275d9e26ba337f7fdac63fcb6c96
CastilhoF <castilho.wylde@gmail.com>
2022-04-15 18:51:23 -0300

    add: group module implementation

--- src/core/app/app.module.ts
+++ src/core/app/app.module.ts
@@ -5,6 +5,7 @@ import { UserModule } from 'src/modules/user/module/user.module';
 import { configEnvironmentsValidation } from '../config/validations/config.env.validation';
 import { TypeOrmConfig } from '../config/database/typeorm.config';
 import { EventModule } from 'src/modules/event/module/event.module';
+import { GroupModule } from 'src/modules/groups/module/groups.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { EventModule } from 'src/modules/event/module/event.module';
     TypeOrmModule.forRootAsync(TypeOrmConfig),
     UserModule,
     EventModule,
+    GroupModule,
   ],
 })
 export class AppModule {}
