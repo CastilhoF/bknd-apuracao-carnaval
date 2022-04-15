93a6dfade92ab82556eaaa97c728f2e3a23fe348
CastilhoF <castilho.wylde@gmail.com>
2022-04-15 19:31:44 -0300

    chore: controller groups modified

--- src/modules/groups/controller/groups.controller.ts
+++ src/modules/groups/controller/groups.controller.ts
@@ -6,7 +6,6 @@ import {
   Param,
   Post,
   Patch,
-  Query,
   UseGuards,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
@@ -36,4 +35,32 @@ export class GroupController {
     );
     return this.groupService.createGroup(createGroupDto);
   }
+
+  // @Get()
+  // findAllGroups(@GetUser() user: User): Promise<Group[]> {
+  //   this.logger.verbose(`User "${user.username}" find all groups.`);
+  //   return this.groupService.findAllGroups();
+  // }
+
+  // @Get(':id')
+  // findOneGroup(@Param('id') id: string, @GetUser() user: User): Promise<Group> {
+  //   this.logger.verbose(`User "${user.username}" find one group id: ${id}`);
+  //   return this.groupService.findOneGroup(id);
+  // }
+
+  // @Patch(':id')
+  // updateGroup(
+  //   @GetUser() user: User,
+  //   @Param('id') id: string,
+  //   @Body() createGroupDto: CreateGroupDto,
+  // ): Promise<Group> {
+  //   this.logger.verbose(`User "${user.username}" update group id: ${id}`);
+  //   return this.groupService.updateGroup(id, createGroupDto);
+  // }
+
+  // @Delete(':id')
+  // deleteGroup(@GetUser() user: User, @Param('id') id: string): Promise<void> {
+  //   this.logger.verbose(`User "${user.username}" delete group id: ${id}`);
+  //   return this.groupService.deleteGroup(id);
+  // }
 }
