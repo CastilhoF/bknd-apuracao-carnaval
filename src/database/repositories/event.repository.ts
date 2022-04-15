017452edf6955916b66b915c79e2d367296f52cf
CastilhoF <castilho.wylde@gmail.com>
2022-04-15 12:13:07 -0300

    add: update method implemented on repository

--- src/database/repositories/event.repository.ts
+++ src/database/repositories/event.repository.ts
@@ -7,9 +7,12 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { FormatDateAndTime } from 'src/utils/format.date';
+import { Logger } from '@nestjs/common';
 
 @EntityRepository(Event)
 export class EventRepository extends Repository<Event> {
+  private logger = new Logger('EventRepository');
+
   async createEvent(createEventDto: CreateEventDto): Promise<Event> {
     const { name, location } = createEventDto;
 
@@ -34,5 +37,34 @@ export class EventRepository extends Repository<Event> {
 
   async findAllEvents(): Promise<Event[]> {
     return await this.find();
+  }
+
+  async updateEvent(
+    id: string,
+    createEventDto: CreateEventDto,
+  ): Promise<Event> {
+    const { name, location } = createEventDto;
+
+    const date = new Date();
+
+    const updatedAt = FormatDateAndTime(date);
+
+    if (!id) {
+      this.logger.error(`Event id is required.`);
+      throw new NotFoundException(`No id provided!`);
+    }
+
+    const event = await this.findOne(id);
+
+    if (!event) {
+      this.logger.error(`Event id "${id}" not found.`);
+      throw new NotFoundException(`Event with ID "${id}" not found`);
+    }
+
+    event.name = name;
+    event.location = location;
+    event.updatedAt = updatedAt;
+
+    return await this.save(event);
   }
 }
