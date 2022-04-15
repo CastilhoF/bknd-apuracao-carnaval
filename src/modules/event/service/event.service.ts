9ff8372e69ed0bf1fc59fc4157310cf7183268e6
CastilhoF <castilho.wylde@gmail.com>
2022-04-15 10:30:14 -0300

    add: service for create event

---
+++ src/modules/event/service/event.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Event } from '../../../database/entities/event.entity';
+import { CreateEventDto } from '../dtos/create.event.dto';
+import { EventRepository } from '../../../database/repositories/event.repository';
+import { User } from 'src/database/entities/user.entity';
+
+@Injectable()
+export class EventService {
+  constructor(
+    @InjectRepository(EventRepository)
+    private eventRepository: EventRepository,
+  ) {}
+
+  async createEvent(createEventDto: CreateEventDto): Promise<Event> {
+    return this.eventRepository.createEvent(createEventDto);
+  }
+}
