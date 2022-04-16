2552da97fd09a9d61198853b837935a32a4641c3
CastilhoF <castilho.wylde@gmail.com>
2022-04-16 16:28:25 -0300

    add: implemented code in judges module file

---
+++ src/modules/judges/module/judges.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JudgesService } from '../service/judges.service';
+import { JudgesRepository } from '../../../database/repositories/judges.repository';
+import { UserModule } from '../../../modules/user/module/user.module';
+import { JudgesController } from '../controller/judges.controller';
+import { QuestionsRepository } from 'src/database/repositories/question.repository';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([JudgesRepository, QuestionsRepository]),
+    UserModule,
+  ],
+  controllers: [JudgesController],
+  providers: [JudgesService],
+})
+export class JudgesModule {}
