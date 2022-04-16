284030a41581f404994174e955ea08cd0b1bc737
CastilhoF <castilho.wylde@gmail.com>
2022-04-16 17:31:46 -0300

    chore: code of import modules corretion

--- src/database/entities/event.entity.ts
+++ src/database/entities/event.entity.ts
@@ -1,5 +1,4 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
-import { Group } from './groups.entity';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Event {
