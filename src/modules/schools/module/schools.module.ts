4b2d04237d1b805d366d8bb7e76a4f7252e16803
CastilhoF <castilho.wylde@gmail.com>
2022-04-17 08:53:36 -0300

    chore: change import source on schools module

--- src/modules/schools/module/schools.module.ts
+++ src/modules/schools/module/schools.module.ts
@@ -4,7 +4,7 @@ import { SchoolsService } from '../service/schools.service';
 import { SchoolsRepository } from '../../../database/repositories/schools.repository';
 import { UserModule } from '../../../modules/user/module/user.module';
 import { SchoolsController } from '../controller/schools.controller';
-import { GroupRepository } from 'src/database/repositories/groups.repository';
+import { GroupRepository } from '../../../database/repositories/groups.repository';
 
 @Module({
   imports: [
