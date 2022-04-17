8fb21ec914736ff711b9fa6ec1fce238ae9f51a2
CastilhoF <castilho.wylde@gmail.com>
2022-04-17 08:53:13 -0300

    chore: change import source on questions module

--- src/modules/questions/module/questions.module.ts
+++ src/modules/questions/module/questions.module.ts
@@ -4,7 +4,7 @@ import { QuestionsService } from '../service/questions.service';
 import { QuestionsRepository } from '../../../database/repositories/question.repository';
 import { UserModule } from '../../../modules/user/module/user.module';
 import { QuestionsController } from '../controller/questions.controller';
-import { JudgesRepository } from 'src/database/repositories/judges.repository';
+import { JudgesRepository } from '../../../database/repositories/judges.repository';
 
 @Module({
   imports: [
