e6f426c4eb631ddd03f0e6ce04ec5a6cd1f1057a
CastilhoF <castilho.wylde@gmail.com>
2022-04-17 08:05:28 -0300

    add: verification module implemented in app module

--- src/core/app/app.module.ts
+++ src/core/app/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { UserModule } from 'src/modules/user/module/user.module';
+import { UserModule } from '../../modules/user/module/user.module';
 import { configEnvironmentsValidation } from '../config/validations/config.env.validation';
 import { TypeOrmConfig } from '../config/database/typeorm.config';
-import { EventModule } from 'src/modules/event/module/event.module';
-import { GroupModule } from 'src/modules/groups/module/groups.module';
-import { SchoolsModule } from 'src/modules/schools/module/schools.module';
-import { JudgesModule } from 'src/modules/judges/module/judges.module';
-import { QuestionsModule } from 'src/modules/questions/module/questions.module';
+import { EventModule } from '../../modules/event/module/event.module';
+import { GroupModule } from '../../modules/groups/module/groups.module';
+import { SchoolsModule } from '../../modules/schools/module/schools.module';
+import { JudgesModule } from '../../modules/judges/module/judges.module';
+import { QuestionsModule } from '../../modules/questions/module/questions.module';
+import { VerificationModule } from '../../modules/verification/module/verification.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { QuestionsModule } from 'src/modules/questions/module/questions.module';
     SchoolsModule,
     JudgesModule,
     QuestionsModule,
+    VerificationModule,
   ],
 })
 export class AppModule {}
