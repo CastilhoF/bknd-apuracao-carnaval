fa81907e25112a086e2c93c6db8744d2abda0743
CastilhoF <castilho.wylde@gmail.com>
2022-04-19 19:02:30 -0300

    add: cors activate

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   const config = swaggerConfig;
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
