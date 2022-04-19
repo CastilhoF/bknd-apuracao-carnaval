efb646e98d9d2f4f06317ddb4f9fa9897ff750e3
CastilhoF <castilho.wylde@gmail.com>
2022-04-19 13:49:25 -0300

    add: new repository of category items module

---
+++ src/database/repositories/category.item.repository.ts
@@ -0,0 +1,107 @@
+import { EntityRepository, Repository, Connection } from 'typeorm';
+import { CategoryItem } from '../entities/category.item.entity';
+import { CreateCategoryItemDto } from '../../modules/categories/dtos/create.category.items.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { FormatDateAndTime } from '../../utils/format.date';
+import { Logger } from '@nestjs/common';
+import { Category } from '../entities/category.entity';
+import { Judges } from '../entities/judges.entity';
+import { Event } from '../entities/event.entity';
+
+@EntityRepository(CategoryItem)
+export class CategoryItemRepository extends Repository<CategoryItem> {
+  private logger = new Logger('CategoryItemRepository');
+  private categoryRepository: Repository<Category>;
+  private judgeRepository: Repository<Judges>;
+  private eventRepository: Repository<Event>;
+
+  constructor(private connection: Connection) {
+    super();
+    this.categoryRepository = this.connection.getRepository<Category>(Category);
+    this.judgeRepository = this.connection.getRepository<Judges>(Judges);
+    this.eventRepository = this.connection.getRepository<Event>(Event);
+  }
+
+  async createCategoryItem(
+    createCategoryItemDto: CreateCategoryItemDto,
+  ): Promise<CategoryItem> {
+    const { category, judges, event } = createCategoryItemDto;
+
+    const createdAt = FormatDateAndTime(new Date());
+
+    const updatedAt = FormatDateAndTime(new Date());
+
+    const categoryObj = await this.categoryRepository.findOne(category.id);
+
+    const judgesArray = [];
+    judges.map(async (judge) => {
+      const result = await this.judgeRepository.findOne(judge.id);
+      judgesArray.push(result);
+    });
+    const eventObj = await this.eventRepository.findOne(event.id);
+
+    const categoryItem = this.create({
+      category: categoryObj,
+      judges: judgesArray,
+      event: eventObj,
+      createdAt,
+      updatedAt,
+    });
+
+    try {
+      return await this.save(categoryItem);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Category name already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  async findAllCategoryItems(): Promise<CategoryItem[]> {
+    return await this.find();
+  }
+
+  async findOneCategoryItem(id: string): Promise<CategoryItem> {
+    return await this.findOne(id);
+  }
+
+  async updateCategoryItem(
+    id: string,
+    createCategoryItemDto: CreateCategoryItemDto,
+  ): Promise<CategoryItem> {
+    const { category, judges, event } = createCategoryItemDto;
+
+    const updatedAt = FormatDateAndTime(new Date());
+
+    const categoryItem = await this.findOne(id);
+
+    if (!categoryItem) {
+      this.logger.error(`CategoryItem id "${id}" not found.`);
+      throw new NotFoundException(`CategoryItem with ID "${id}" not found`);
+    }
+
+    categoryItem.category = category;
+    categoryItem.judges = judges;
+    categoryItem.event = event;
+    categoryItem.updatedAt = updatedAt;
+
+    try {
+      return await this.save(categoryItem);
+    } catch (error) {
+      throw new InternalServerErrorException();
+    }
+  }
+
+  async deleteCategoryItem(id: string): Promise<void> {
+    const result = await this.delete(id);
+    if (result.affected === 0) {
+      throw new NotFoundException(`CategoryItem with ID "${id}" not found`);
+    }
+  }
+}
