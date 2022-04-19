9ec8bc04a1c46b91e3c90798c6afac009403036c
CastilhoF <castilho.wylde@gmail.com>
2022-04-19 13:46:47 -0300

    chore: change status code of response signin method

--- src/modules/user/controller/user.controller.ts
+++ src/modules/user/controller/user.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, HttpStatus, Res } from '@nestjs/common';
 import { UserService } from '../service/user.service';
 import { UserCredentialsDto } from '../dtos/user.dto';
+import { Response } from 'express';
 
 @Controller('user')
 export class UserController {
@@ -12,7 +13,11 @@ export class UserController {
   }
 
   @Post('signin')
-  async signIn(@Body() userCredentials: UserCredentialsDto) {
+  async signIn(
+    @Res({ passthrough: true }) res: Response,
+    @Body() userCredentials: UserCredentialsDto,
+  ) {
+    res.status(HttpStatus.OK);
     return this.userService.signIn(userCredentials);
   }
 }
