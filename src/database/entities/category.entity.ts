1b08bf6f5e2f9adfd3fa081e253c946062cded1d
CastilhoF <castilho.wylde@gmail.com>
2022-04-19 13:47:50 -0300

    add: new entity of category module

---
+++ src/database/entities/category.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Category {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  name: string;
+
+  @Column()
+  createdAt: string;
+
+  @Column()
+  updatedAt: string;
+}
