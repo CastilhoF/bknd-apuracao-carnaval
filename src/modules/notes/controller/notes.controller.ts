5762f2b2fd1dd6506bbc57b94ee19b944f14cfca
CastilhoF <castilho.wylde@gmail.com>
2022-04-19 18:05:39 -0300

    chore: modify notes controller to fix status code for all methods

--- src/modules/notes/controller/notes.controller.ts
+++ src/modules/notes/controller/notes.controller.ts
@@ -5,7 +5,9 @@ import {
   Get,
   Param,
   Post,
-  Patch,
+  HttpStatus,
+  Res,
+  Put,
   UseGuards,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
@@ -14,6 +16,7 @@ import { User } from '../../../database/entities/user.entity';
 import { Notes } from '../../../database/entities/notes.entity';
 import { NotesService } from '../service/notes.service';
 import { Logger } from '@nestjs/common';
+import { Response } from 'express';
 
 @Controller('notes')
 @UseGuards(AuthGuard())
@@ -26,18 +29,24 @@ export class NotesController {
   async createNote(
     @Body() createNoteDto: Notes,
     @GetUser() user: User,
+    @Res({ passthrough: true }) res: Response,
   ): Promise<Notes> {
     this.logger.verbose(
       `User "${user.username}" creating a note. Data: ${JSON.stringify(
         createNoteDto,
       )}`,
     );
+    res.status(HttpStatus.CREATED);
     return this.notesService.createNote(createNoteDto);
   }
 
   @Get()
-  async findAllNotes(@GetUser() user: User): Promise<Notes[]> {
+  async findAllNotes(
+    @Res({ passthrough: true }) res: Response,
+    @GetUser() user: User,
+  ): Promise<Notes[]> {
     this.logger.verbose(`User "${user.username}" retrieving all notes.`);
+    res.status(HttpStatus.OK);
     return this.notesService.findAllNotes();
   }
 
@@ -45,24 +54,28 @@ export class NotesController {
   async findOneNote(
     @Param('id') id: string,
     @GetUser() user: User,
+    @Res({ passthrough: true }) res: Response,
   ): Promise<Notes> {
     this.logger.verbose(
       `User "${user.username}" retrieving note with ID "${id}".`,
     );
+    res.status(HttpStatus.OK);
     return this.notesService.findOneNote(id);
   }
 
-  @Patch(':id')
+  @Put(':id')
   async updateNote(
     @Param('id') id: string,
     @Body() createNoteDto: Notes,
     @GetUser() user: User,
+    @Res({ passthrough: true }) res: Response,
   ): Promise<Notes> {
     this.logger.verbose(
       `User "${
         user.username
       }" updating note with ID "${id}". Data: ${JSON.stringify(createNoteDto)}`,
     );
+    res.status(HttpStatus.OK);
     return this.notesService.updateNote(id, createNoteDto);
   }
 
@@ -70,10 +83,12 @@ export class NotesController {
   async deleteNote(
     @Param('id') id: string,
     @GetUser() user: User,
+    @Res({ passthrough: true }) res: Response,
   ): Promise<void> {
     this.logger.verbose(
       `User "${user.username}" deleting note with ID "${id}".`,
     );
+    res.status(HttpStatus.NO_CONTENT);
     return this.notesService.deleteNote(id);
   }
 }
