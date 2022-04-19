efce618403c833ef03c24c6d4ae395e016294dce
CastilhoF <castilho.wylde@gmail.com>
2022-04-19 13:43:50 -0300

    chore: implementation school module

--- src/modules/schools/module/schools.module.ts
+++ src/modules/schools/module/schools.module.ts
@@ -4,13 +4,9 @@ import { SchoolsService } from '../service/schools.service';
 import { SchoolsRepository } from '../../../database/repositories/schools.repository';
 import { UserModule } from '../../../modules/user/module/user.module';
 import { SchoolsController } from '../controller/schools.controller';
-import { GroupRepository } from '../../../database/repositories/groups.repository';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([SchoolsRepository, GroupRepository]),
-    UserModule,
-  ],
+  imports: [TypeOrmModule.forFeature([SchoolsRepository]), UserModule],
   controllers: [SchoolsController],
   providers: [SchoolsService],
 })
