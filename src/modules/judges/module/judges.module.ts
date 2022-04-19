c3127e30d47f2110ef2743d2dff91f19e32e9286
CastilhoF <castilho.wylde@gmail.com>
2022-04-19 13:41:52 -0300

    chore: changed insertions of judges modules

--- src/modules/judges/module/judges.module.ts
+++ src/modules/judges/module/judges.module.ts
@@ -4,13 +4,9 @@ import { JudgesService } from '../service/judges.service';
 import { JudgesRepository } from '../../../database/repositories/judges.repository';
 import { UserModule } from '../../../modules/user/module/user.module';
 import { JudgesController } from '../controller/judges.controller';
-import { QuestionsRepository } from '../../../database/repositories/question.repository';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([JudgesRepository, QuestionsRepository]),
-    UserModule,
-  ],
+  imports: [TypeOrmModule.forFeature([JudgesRepository]), UserModule],
   controllers: [JudgesController],
   providers: [JudgesService],
 })
