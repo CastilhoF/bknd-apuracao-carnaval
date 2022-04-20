19f9fb91c671623d2b22375eb59dbb00db9b5349
CastilhoF <castilho.wylde@gmail.com>
2022-04-20 08:49:44 -0300

    add: create penalty method on penalties service file

---
+++ src/modules/penalties/service/penalties.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Penalties } from '../../../database/entities/penalties.entity';
+import { CreatePenaltiesDto } from '../../../modules/penalties/dtos/create.penalties.dto';
+import { PenaltiesRepository } from '../../../database/repositories/penalties.repository';
+
+@Injectable()
+export class PenaltiesService {
+  constructor(
+    @InjectRepository(PenaltiesRepository)
+    private readonly penaltiesRepository: PenaltiesRepository,
+  ) {}
+
+  async createPenalties(
+    createPenaltiesDto: CreatePenaltiesDto,
+  ): Promise<Penalties> {
+    return this.penaltiesRepository.createPenalties(createPenaltiesDto);
+  }
+
+  // async findAllPenalties(): Promise<Penalties[]> {
+  //   return this.penaltiesRepository.findAllPenalties();
+  // }
+
+  // async findOnePenalties(id: string): Promise<Penalties> {
+  //   return await this.penaltiesRepository.findPenaltiesById(id);
+  // }
+
+  // async updatePenalties(
+  //   id: string,
+  //   createPenaltiesDto: CreatePenaltiesDto,
+  // ): Promise<Penalties> {
+  //   return await this.penaltiesRepository.updatePenalties(
+  //     id,
+  //     createPenaltiesDto,
+  //   );
+  // }
+
+  // async deletePenalties(id: string): Promise<void> {
+  //   return this.penaltiesRepository.deletePenalties(id);
+  // }
+}
