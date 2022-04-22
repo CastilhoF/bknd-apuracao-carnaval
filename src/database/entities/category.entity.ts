49968dd33e381a7d5929e925af90671a6a9c502b
CastilhoF <castilho.wylde@gmail.com>
2022-04-22 11:03:49 -0300

    chore: imementation swagger api documentation - category entity

--- src/database/entities/category.entity.ts
+++ src/database/entities/category.entity.ts
@@ -1,20 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { CategoryItem } from './category.item.entity';
 
 @Entity()
 export class Category {
+  @ApiProperty()
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ApiProperty()
   @Column({ unique: true })
   name: string;
 
   @OneToMany((_type) => CategoryItem, (categoryItem) => categoryItem.category)
   categoryItems: CategoryItem[];
 
+  @ApiProperty()
   @Column()
   createdAt: string;
 
+  @ApiProperty()
   @Column()
   updatedAt: string;
 }
