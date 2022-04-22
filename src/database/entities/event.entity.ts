d835299d701aca9b6bb02aa10c78a398e120a8ed
CastilhoF <castilho.wylde@gmail.com>
2022-04-22 11:04:22 -0300

    chore: imementation swagger api documentation - event entity

--- src/database/entities/event.entity.ts
+++ src/database/entities/event.entity.ts
@@ -2,49 +2,63 @@ import { Column, Entity, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 import { Penalties } from './penalties.entity';
 import { CategoryItem } from './category.item.entity';
 import { Notes } from './notes.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class Event {
+  @ApiProperty()
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ApiProperty()
   @Column()
   name: string;
 
+  @ApiProperty()
   @Column()
   city: string;
 
+  @ApiProperty()
   @Column()
   year: number;
 
+  @ApiProperty()
   @Column()
   champions: number;
 
+  @ApiProperty()
   @Column()
   demotes: number;
 
+  @ApiProperty()
   @Column()
   discard_min: boolean;
 
+  @ApiProperty()
   @Column()
   discard_max: boolean;
 
+  @ApiProperty()
   @OneToMany((_type) => CategoryItem, (categoryItem) => categoryItem.event, {
     eager: true,
   })
   categoryItem: CategoryItem[];
 
+  @ApiProperty()
   @OneToMany((_types) => Notes, (notes) => notes.event, { eager: true })
   notes: Notes[];
 
+  @ApiProperty()
   @OneToMany((_types) => Penalties, (penalties) => penalties.event, {
     eager: true,
   })
   penalties: Penalties[];
 
+  @ApiProperty()
   @Column()
   createdAt: string;
 
+  @ApiProperty()
   @Column()
   updatedAt: string;
 }
