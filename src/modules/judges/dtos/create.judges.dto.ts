da2015037a5358d0499df8d71a1f214c2acaf8da
CastilhoF <castilho.wylde@gmail.com>
2022-04-22 11:14:26 -0300

    chore: imementation swagger api documentation - judge module - create judge dto

--- src/modules/judges/dtos/create.judges.dto.ts
+++ src/modules/judges/dtos/create.judges.dto.ts
@@ -1,6 +1,8 @@
 import { IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateJudgesDto {
   @IsNotEmpty()
+  @ApiProperty()
   name: string;
 }
