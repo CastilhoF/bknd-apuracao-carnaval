c439063c574a41d0c501447f5af487dc85decf91
CastilhoF <castilho.wylde@gmail.com>
2022-04-22 11:11:10 -0300

    chore: imementation swagger api documentation - event module - event controller file

--- src/modules/event/controller/event.controller.ts
+++ src/modules/event/controller/event.controller.ts
@@ -18,14 +18,31 @@ import { Event } from '../../../database/entities/event.entity';
 import { EventService } from '../service/event.service';
 import { Logger } from '@nestjs/common';
 import { Response } from 'express';
+import {
+  ApiBody,
+  ApiCreatedResponse,
+  ApiNoContentResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+import { CreatedEventDto } from '../../../core/config/documentation/dtos/created/created.event.dto';
+import { EventDto } from '../../../core/config/documentation/dtos/single-objects/event';
 
 @Controller('events')
+@ApiTags('Events')
 export class EventController {
   private logger = new Logger('EventController');
 
   constructor(private eventService: EventService) {}
 
   @Post()
+  @ApiBody({ type: CreateEventDto })
+  @ApiOperation({ summary: 'Create Events. Ex. (Carnivals)' })
+  @ApiCreatedResponse({
+    description: `User "USERNAME" create a new event. Event: { Created Event Dto }`,
+    type: CreatedEventDto,
+  })
   @UseGuards(AuthGuard())
   createEvent(
     @Body() createEventDto: CreateEventDto,
@@ -42,6 +59,11 @@ export class EventController {
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get All Events' })
+  @ApiOkResponse({
+    description: 'Successfully',
+    type: [EventDto],
+  })
   findAllEvents(
     @Res({ passthrough: true }) res: Response,
     // @GetUser() user: User,
@@ -52,6 +74,8 @@ export class EventController {
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get Event By ID' })
+  @ApiOkResponse({ description: 'Successfully', type: EventDto })
   findOneEvent(
     @Res({ passthrough: true }) res: Response,
     @Param('id') id: string,
@@ -63,6 +87,8 @@ export class EventController {
   }
 
   @Put(':id')
+  @ApiOperation({ summary: 'Update Event By ID' })
+  @ApiOkResponse({ description: 'Event Updated Successfully', type: EventDto })
   @UseGuards(AuthGuard())
   updateEvent(
     @Res({ passthrough: true }) res: Response,
@@ -76,6 +102,8 @@ export class EventController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete Event By ID' })
+  @ApiNoContentResponse({ description: 'Event Deleted Successfully' })
   @UseGuards(AuthGuard())
   deleteEvent(
     @Res({ passthrough: true }) res: Response,
