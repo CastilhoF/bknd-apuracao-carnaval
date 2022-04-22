6481aa718683fca0ba646093f3430c8aeeb43a9f
CastilhoF <castilho.wylde@gmail.com>
2022-04-22 11:18:35 -0300

    chore: imementation swagger api documentation - schools module - create schools dto file

--- src/modules/schools/dtos/create.schools.dto.ts
+++ src/modules/schools/dtos/create.schools.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 
 export class CreateSchoolsDto {
   @IsNotEmpty()
+  @ApiProperty()
   name: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   city: string;
 }
