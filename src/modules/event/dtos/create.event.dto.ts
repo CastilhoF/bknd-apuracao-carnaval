e71e239a3b08975c99e544f4e59b11fa4ed5f0c6
CastilhoF <castilho.wylde@gmail.com>
2022-04-22 11:11:45 -0300

    chore: imementation swagger api documentation - event module - create event dto file

--- src/modules/event/dtos/create.event.dto.ts
+++ src/modules/event/dtos/create.event.dto.ts
@@ -1,24 +1,32 @@
 import { IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateEventDto {
   @IsNotEmpty()
+  @ApiProperty()
   name: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   city: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   year: number;
 
   @IsNotEmpty()
+  @ApiProperty()
   champions: number;
 
   @IsNotEmpty()
+  @ApiProperty()
   demotes: number;
 
   @IsNotEmpty()
+  @ApiProperty()
   discard_min: boolean;
 
   @IsNotEmpty()
+  @ApiProperty()
   discard_max: boolean;
 }
