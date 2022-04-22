62c44bcf11302f74afae98e132385a883b0687b6
CastilhoF <castilho.wylde@gmail.com>
2022-04-22 11:03:24 -0300

    chore: imementation swagger api documentation - swagger config file

--- src/core/config/documentation/swagger.config.ts
+++ src/core/config/documentation/swagger.config.ts
@@ -2,9 +2,5 @@ import { DocumentBuilder } from '@nestjs/swagger';
 
 export const swaggerConfig = new DocumentBuilder()
   .setTitle('O Dia - Backend - Apuração Carnaval')
-  .setDescription('NestJS API')
   .setVersion('1.0')
-  .addTag('odiabackend')
-  .addTag('apuracao')
-  .addTag('carnaval')
   .build();
