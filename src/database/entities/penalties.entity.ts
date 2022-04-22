ba5f1cc33f3fb14ae0d86d8683478f3067f3cf5d
CastilhoF <castilho.wylde@gmail.com>
2022-04-22 11:05:50 -0300

    chore: imementation swagger api documentation - penalties entity

--- src/database/entities/penalties.entity.ts
+++ src/database/entities/penalties.entity.ts
@@ -1,24 +1,31 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Schools } from './schools.entity';
 import { Event } from './event.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class Penalties {
+  @ApiProperty()
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ApiProperty()
   @ManyToOne((_type) => Event, (event) => event.penalties)
   event: Event;
 
+  @ApiProperty()
   @ManyToOne((_type) => Schools, (school) => school.id, { eager: true })
   school: Schools;
 
+  @ApiProperty()
   @Column()
-  value: number;
+  value: string;
 
+  @ApiProperty()
   @Column()
   createdAt: string;
 
+  @ApiProperty()
   @Column()
   updatedAt: string;
 }
