e2cc9b1cb6a5ee68b30871f41fc2b92fb2d9f1f9
CastilhoF <castilho.wylde@gmail.com>
2022-04-22 11:09:22 -0300

    chore: imementation swagger api documentation - category module - create category dto file

--- src/modules/categories/dtos/create.category.dto.ts
+++ src/modules/categories/dtos/create.category.dto.ts
@@ -1,6 +1,8 @@
 import { IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCategoryDto {
   @IsNotEmpty()
+  @ApiProperty()
   name: string;
 }
