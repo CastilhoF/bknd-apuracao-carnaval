e571a7e75004237488efb297e4b4878e990afd9a
CastilhoF <castilho.wylde@gmail.com>
2022-04-22 11:06:38 -0300

    chore: imementation swagger api documentation - user entity

--- src/database/entities/user.entity.ts
+++ src/database/entities/user.entity.ts
@@ -1,19 +1,25 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class User {
+  @ApiProperty()
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ApiProperty()
   @Column({ unique: true })
   username: string;
 
+  @ApiProperty()
   @Column()
   password: string;
 
+  @ApiProperty()
   @Column()
   createdAt: Date;
 
+  @ApiProperty()
   @Column()
   updatedAt: Date;
 }
