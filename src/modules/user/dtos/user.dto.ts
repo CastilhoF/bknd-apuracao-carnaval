33fd8d51bf41d9b84d7f22fed78a85a62b7503f7
CastilhoF <castilho.wylde@gmail.com>
2022-04-25 08:50:29 -0300

    chore: add notations for swagger documentation - create user dto

--- src/modules/user/dtos/user.dto.ts
+++ src/modules/user/dtos/user.dto.ts
@@ -5,7 +5,11 @@ export class UserCredentialsDto {
   @IsString()
   @MinLength(4, { message: 'User must contain more than 4 characters.' })
   @MaxLength(255, { message: 'The User must contain up to 255 characters.' })
-  @ApiProperty()
+  @ApiProperty({
+    description:
+      'User name must contain more than 4 characters and up to 255 characters.',
+    example: 'John Doe',
+  })
   username: string;
 
   @IsString()
@@ -24,6 +28,10 @@ export class UserCredentialsDto {
     message:
       'Password is to weak! Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 number or special character.',
   })
-  @ApiProperty()
+  @ApiProperty({
+    description:
+      'Password must contain at least 8 characters and cannot exceed a maximum of 32 characters. Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 number or special character.',
+    example: 'P@ssw0rd',
+  })
   password: string;
 }
