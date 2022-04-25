9d010950ef775e2ea64e72ea75e2d9faca826afa
CastilhoF <castilho.wylde@gmail.com>
2022-04-25 08:43:03 -0300

    chore: change swagger configuration

--- src/core/config/documentation/swagger.config.ts
+++ src/core/config/documentation/swagger.config.ts
@@ -3,4 +3,9 @@ import { DocumentBuilder } from '@nestjs/swagger';
 export const swaggerConfig = new DocumentBuilder()
   .setTitle('O Dia - Backend - Apuração Carnaval')
   .setVersion('1.0')
+  .addBearerAuth({
+    type: 'http',
+    scheme: 'bearer',
+    bearerFormat: 'JWT',
+  })
   .build();
